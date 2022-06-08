34200c03263156cd12f8319ea0fa301b02083899
Thanzeel Jalaldeen <thanzeel@inexisconsulting.com>
2022-06-08 13:29:23 +0530

    added nested validation

--- POC.Application/Features/Users/Command/CreateUser/CreateUserCommandValidator.cs
+++ POC.Application/Features/Users/Command/CreateUser/CreateUserCommandValidator.cs
@@ -1,37 +1,37 @@
 ﻿using FluentValidation;
 using static POC.Utility.BaseEnums;
 
-namespace POC.Application.Features.Users.Command.CreateUser
+namespace POC.Application.Features.Users.Command.CreateUser;
+
+public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
 {
-    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    public CreateUserCommandValidator()
     {
-        public CreateUserCommandValidator()
-        {
-            RuleFor(r => r.FirstName)
-                .NotEmpty().WithMessage("{PropertyName} is required")
-                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+        RuleFor(r => r.FirstName)
+            .NotEmpty().WithMessage("{PropertyName} is required")
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
 
 
-            RuleFor(r => r.LastName)
-                .NotEmpty().WithMessage("{PropertyName} is required")
-                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+        RuleFor(r => r.LastName)
+            .NotEmpty().WithMessage("{PropertyName} is required")
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
 
 
-            RuleFor(r => r.Address)
-                .NotEmpty().WithMessage("{PropertyName} is required")
-                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 900 characters.");
+        RuleFor(r => r.Address)
+            .NotEmpty().WithMessage("{PropertyName} is required")
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed 900 characters.");
 
 
-            RuleFor(r => r.School)
-              .NotEmpty().WithMessage("{PropertyName} is required")
-              .MaximumLength(50).WithMessage("{PropertyName} must not exceed 200 characters.");
+        RuleFor(r => r.School)
+          .NotEmpty().WithMessage("{PropertyName} is required")
+          .MaximumLength(50).WithMessage("{PropertyName} must not exceed 200 characters.");
 
-            RuleFor(r => r.Gender)
-                .IsInEnum().NotEqual(Gender.None).WithName("Gender").WithMessage("{PropertyName} invalid gender");
-            
-            RuleFor(r => r.Grade.Name)
-                .NotEmpty().WithMessage("invalid grade name")
-                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters."); ;
-        }
+        RuleFor(r => r.Gender)
+            .IsInEnum().NotEqual(Gender.None).WithName("Gender").WithMessage("{PropertyName} invalid gender");
+
+        RuleFor(x => x.Grade)
+                .NotEmpty()
+              .SetValidator(new GradeValidator());
+
     }
 }
---
+++ POC.Application/Features/Users/Command/CreateUser/GradeValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace POC.Application.Features.Users.Command.CreateUser;
+
+public class GradeValidator : AbstractValidator<Grade>
+{
+    public GradeValidator()
+    {
+        RuleFor(r => r.Name)
+           .NotEmpty().WithMessage("invalid grade name")
+           .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters."); ;
+    }
+}
