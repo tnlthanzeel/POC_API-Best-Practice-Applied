48be4fed5dfef244326d9f42708d9abe61857b1e
Thanzeel Jalaldeen <thanzeel@inexisconsulting.com>
2022-06-27 09:52:39 +0530

    changed program.cs to top level statement and removed Startup.cs

--- POC.Api/Program.cs
+++ POC.Api/Program.cs
@@ -1,28 +1,74 @@
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using POC.Api;
+using POC.Api.Middleware;
+using POC.Application;
+using POC.Infrastructure;
+using POC.Persistence;
 using Serilog;
+using Serilog.Events;
 
-namespace POC.Api;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+#if DEBUG
+Log.Logger = new LoggerConfiguration()
+   .ReadFrom.Configuration(builder.Configuration)
+   .WriteTo.Debug()
+   .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs/log-.txt"), restrictedToMinimumLevel: LogEventLevel.Error, rollingInterval: RollingInterval.Day)
+   .CreateLogger();
+#else
+Log.Logger = new LoggerConfiguration()
+   .MinimumLevel.Error()
+   .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs/log-.txt"), rollingInterval: RollingInterval.Day)
+   .CreateLogger();
+#endif
+
+//var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+
+var services = builder.Services;
+
+services.AddApplicationServices();
+services.AddInfrastructureServices(builder.Configuration);
+services.AddPersistenceServices(builder.Configuration);
+
+services.AddCors(options =>
 {
-    public static void Main(string[] args)
-    {
-        var config = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json")
-            .Build();
+    options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
 
-        Log.Logger = new LoggerConfiguration()
-           .ReadFrom.Configuration(config)
-           .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs/log-.txt"), rollingInterval: RollingInterval.Day)
-           .CreateLogger();
+services.AddControllerConfig();
 
-        CreateHostBuilder(args).Build().Run();
-    }
+services.AddFluentValidationRulesToSwagger();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-        .UseSerilog()
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+services.AddSwagger();
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+
+app.UseCustomExceptionHandler();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "POC API");
+});
+
+app.UseHttpsRedirection();
+
+app.UseCors("Open");
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
