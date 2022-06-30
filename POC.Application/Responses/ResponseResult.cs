c7979df7a8beba040cac7f5458d67c17cfd1b502
Thanzeel Jalaldeen <thanzeel@inexisconsulting.com>
2022-06-30 15:34:51 +0530

    refactor ResponseResult

--- POC.Application/Responses/ResponseResult.cs
+++ POC.Application/Responses/ResponseResult.cs
@@ -1,7 +1,5 @@
 ﻿using FluentValidation.Results;
 using Newtonsoft.Json;
-using POC.Application.Exceptions;
-using System.Net;
 
 namespace POC.Application.Responses;
 
@@ -12,39 +10,9 @@ public sealed class ResponseResult : ResponseResult<object>
         Success = true;
     }
 
-    public ResponseResult(IList<ValidationFailure> validationFailures) : base(default(object))
-    {
-        HttpStatusCode = HttpStatusCode.BadRequest;
+    public ResponseResult(IList<ValidationFailure> validationFailures) : base(validationFailures) { }
 
-        if (validationFailures.Count is not 0)
-        {
-            Errors.AddRange(validationFailures.Select(s => new KeyValuePair<string, IEnumerable<string>>(s.PropertyName, new[] { s.ErrorMessage })).ToList());
-        }
-    }
-
-    public ResponseResult(ApplicationException ex) : base(default(object))
-    {
-        var errorMsg = new[] { ex.Message };
-
-        switch (ex)
-        {
-            case BadRequestException e:
-                HttpStatusCode = HttpStatusCode.BadRequest;
-                Errors.Add(new KeyValuePair<string, IEnumerable<string>>(e.PropertyName, errorMsg));
-                break;
-
-            case ValidationException e:
-                HttpStatusCode = HttpStatusCode.BadRequest;
-                Errors.AddRange(e.ValdationErrors);
-                break;
-
-            case NotFoundException e:
-                HttpStatusCode = HttpStatusCode.NotFound;
-                Errors.Add(new KeyValuePair<string, IEnumerable<string>>(e.PropertyName, errorMsg));
-                break;
-
-        };
-    }
+    public ResponseResult(ApplicationException ex) : base(ex) { }
 
     [JsonIgnore]
     public override List<KeyValuePair<string, IEnumerable<string>>> Errors { get; init; } = new();
